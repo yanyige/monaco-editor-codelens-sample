795a5d01620bf989bdd89a54c25450ef40f0e984
奕格 <yige.yyg@alibaba-inc.com>
2018-08-30 11:34:15 +0800

    Checked in by yige, update

--- src/provideCodeLenses.ts
+++ src/provideCodeLenses.ts
@@ -11,9 +11,7 @@ let decorations: any[] = [];
 export default function (model: any, editor: any) {
   let conflicts = MergeConflictParser.scanDocument(model);
   let bgColorDecorations = MergeDecorationParser.getColorDecorations(conflicts);
-  console.log('bgColorDecorations', bgColorDecorations);
   decorations = editor.deltaDecorations(decorations, bgColorDecorations);
-  
   let codeLenses = CodeLensesParser(editor, model, conflicts);
             
   return codeLenses;
